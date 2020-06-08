4c1623ff04e7a1730a5dd793d7f231b75669ce89
Thomas Strohmeier <thomas.strohmeier@byteschneiderei.com>
2020-06-08 21:26:32 +0200

    Refactor CI/CD push script

--- scripts/02_push.sh
+++ scripts/02_push.sh
@@ -5,15 +5,15 @@ set -e
 REGISTRY_BASE=tstrohmeier
 
 push_images() {
-  echo "----------  START: Pushing Docker Image $2  ----------"
-  docker push $REGISTRY_BASE/$2
-  echo "----------  END: Pushing Docker Image $2  ----------"
+  echo "----------  START: Pushing Docker Image $1  ----------"
+  docker push $REGISTRY_BASE/$1
+  echo "----------  END: Pushing Docker Image $1  ----------"
 }
 
 
 # Spark Base Image with Hadoop and SBT
-push_images ./docker/spark/2.2.0  spark-development:2.2.0
-push_images ./docker/spark/2.4.5  spark-development:2.4.5
+push_images spark-development:2.2.0
+push_images spark-development:2.4.5
 
 # Derived Images
-push_images ./docker/gradle/6.5  spark-development:2.4.5-gradle-6.5
+push_images spark-development:2.4.5-gradle-6.5
